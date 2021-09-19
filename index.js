a58bd3ab3803bbeda6ec18d2bb3db0808d686dde
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-09-19 18:17:58 -0500

    add input for admin sign up

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello!");
+const express = require('express');
+
+const app = express();
+
+// Route Handler //
+app.get('/', (req, res) => {
+   // greeting to user //
+   res.send(`
+      <div>
+         <form>
+            <input placeholder="email">
+            <input placeholder="password">
+            <input placeholder="password confirmation">
+            <button>Sign Up</button>
+         </form>
+      </div>
+   `);
+});
+
+app.listen(3000, () => {
+   console.log('Listening')
+});
