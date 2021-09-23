c18c30c5f4cf728a764edeab7017880661f3e97a
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-09-22 21:30:43 -0500

    add writeAll and create methods to UsersRepository class

--- repositories/users.js
+++ repositories/users.js
@@ -13,16 +13,32 @@ class UsersRepository {
       }
    }
    
-     async getAll(){
+   async getAll(){
       return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}));
-   }; 
-     
+   };
+
+   async create (attrs) {
+      const records = await this.getAll();
+      records.push(attrs);
+
+      await this.writeAll(records);
+   }
+
+   async writeAll(records) {
+      await fs.promises.writeFile(
+         this.filename, 
+         JSON.stringify(records, null, 2)
+      );
+   }
 }
 
 
 // Test code //
 const test = async () => {
    const repo = new UsersRepository('users.json');
+   
+   await repo.create({ email: 'test@test.com', password: 'password' });
+   
    const users = await repo.getAll();
 
    console.log(users);
