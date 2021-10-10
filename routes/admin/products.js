6002ca35431115128da2af2136ce7e77dd08acb1
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-10-10 15:23:57 -0500

    installed multer for image upload and storage

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() })
 
 // route for admin to list products //
 router.get('/admin/products', (req, res) => {
@@ -16,12 +19,16 @@ router.get('/admin/products/new', (req, res) => {
    res.send(productsNewTemplate({}));
 });
 
-// route handler that handles new product form submission //
+// route handler for new product form submission //
 // 2nd argument is array of validators to run on form submission //
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-   const errors = validationResult(req);
-   console.log(errors);
-   res.send('submitted');
+router.post(
+   '/admin/products/new', 
+   [requireTitle, requirePrice], 
+   upload.single('image'), 
+   (req, res) => {
+      const errors = validationResult(req);
+      console.log(req.file); // stores uploaded image
+      res.send('submitted');
 });
 
 module.exports = router;
